f0904d30c822df47fcecd1b8a33b0e999695b1e0
Luke Hatcher <lukeman@gmail.com>
2011-09-09 15:11:59 -0700

    indent and comment out theme javascript for now

--- pinax_theme_bootstrap/static/js/theme.js
+++ pinax_theme_bootstrap/static/js/theme.js
@@ -1,6 +1,7 @@
 $(function(){
-$("a[rel=popover]").popover({offset:10});
-$("a.tip").twipsy({live:true});
-$(".alert-message").alert();
-$("#topbar").dropdown();
+    // $("a[rel=popover]").popover({offset:10});
+    // $("a.tip").twipsy({live:true});
+    // $(".alert-message").alert();
+    // $("#topbar").dropdown();
 });
+
