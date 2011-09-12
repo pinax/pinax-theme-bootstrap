4ea16b32a72c678109603ab3ab97331761a346c7
Luke Hatcher <lukeman@gmail.com>
2011-09-12 10:53:13 -0700

    adding dropdown to account bar

--- pinax_theme_bootstrap/static/js/theme.js
+++ pinax_theme_bootstrap/static/js/theme.js
@@ -2,7 +2,7 @@ $(function(){
     // $("a[rel=popover]").popover({offset:10});
     // $("a.tip").twipsy({live:true});
     // $(".alert-message").alert();
-    // $("#topbar").dropdown();
+    $(".topbar").dropdown();
 });
 
 $(document).ajaxSend(function(event, xhr, settings) {
