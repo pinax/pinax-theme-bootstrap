02d6190c0d0f1bdad14f91d7a6d1b240ce5f7d6b
Luke Hatcher <lukeman@gmail.com>
2011-09-18 17:26:50 -0400

    adding bootstrap 1.3 assets (css and js plugins)

--- pinax_theme_bootstrap/static/js/bootstrap-alerts.js
+++ pinax_theme_bootstrap/static/js/bootstrap-alerts.js
@@ -1,61 +1,76 @@
+/* ==========================================================
+ * bootstrap-alerts.js v1.3.0
+ * http://twitter.github.com/bootstrap/javascript.html#alerts
+ * ==========================================================
+ * Copyright 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
+
+
 (function( $ ){
 
   /* CSS TRANSITION SUPPORT (https://gist.github.com/373874)
    * ======================================================= */
 
-  $.support.transition = (function () {
-    var thisBody = document.body || document.documentElement
-      , thisStyle = thisBody.style
-      , support = thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined
-    return support
-  })()
-
-
- /* SHARED VARS
-  * =========== */
-
    var transitionEnd
 
-  // set CSS transition event type
-  if ( $.support.transition ) {
-    transitionEnd = "TransitionEnd"
-    if ( $.browser.webkit ) {
-    	transitionEnd = "webkitTransitionEnd"
-    } else if ( $.browser.mozilla ) {
-    	transitionEnd = "transitionend"
-    } else if ( $.browser.opera ) {
-    	transitionEnd = "oTransitionEnd"
-    }
-  }
+   $(document).ready(function () {
 
+     $.support.transition = (function () {
+       var thisBody = document.body || document.documentElement
+         , thisStyle = thisBody.style
+         , support = thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined
+       return support
+     })()
+
+     // set CSS transition event type
+     if ( $.support.transition ) {
+       transitionEnd = "TransitionEnd"
+       if ( $.browser.webkit ) {
+       	transitionEnd = "webkitTransitionEnd"
+       } else if ( $.browser.mozilla ) {
+       	transitionEnd = "transitionend"
+       } else if ( $.browser.opera ) {
+       	transitionEnd = "oTransitionEnd"
+       }
+     }
+
+   })
 
  /* ALERT CLASS DEFINITION
   * ====================== */
 
-  var Alert = function ( content ) {
-    var that = this
+  var Alert = function ( content, selector ) {
     this.$element = $(content)
-    this.$element.delegate('.close', 'click', function (e) {
-      e.preventDefault()
-      that.close()
-    })
+      .delegate(selector || '.close', 'click', this.close)
   }
 
   Alert.prototype = {
 
-    close: function () {
-      var that = this
+    close: function (e) {
+      var $element = $(this).parent('.alert-message')
+
+      e && e.preventDefault()
+      $element.removeClass('in')
 
       function removeElement () {
-        that.$element.remove()
-        that.$element = null
+        $element.remove()
       }
 
-      $.support.transition && this.$element.hasClass('fade') ?
-        this.$element.bind(transitionEnd, removeElement) :
+      $.support.transition && $element.hasClass('fade') ?
+        $element.bind(transitionEnd, removeElement) :
         removeElement()
-
-      this.$element.removeClass('in')
     }
 
   }
@@ -65,9 +80,25 @@
   * ======================= */
 
   $.fn.alert = function ( options ) {
+
+    if ( options === true ) {
+      return this.data('alert')
+    }
+
     return this.each(function () {
-      new Alert(this)
+      var $this = $(this)
+
+      if ( typeof options == 'string' ) {
+        return $this.data('alert')[options]()
+      }
+
+      $(this).data('alert', new Alert( this ))
+
     })
   }
 
-})( jQuery || ender )
+  $(document).ready(function () {
+    new Alert($('body'), '.alert-message[data-alert] .close')
+  })
+
+})( window.jQuery || window.ender )
--- pinax_theme_bootstrap/static/js/bootstrap-dropdown.js
+++ pinax_theme_bootstrap/static/js/bootstrap-dropdown.js
@@ -1,22 +1,50 @@
+/* ============================================================
+ * bootstrap-dropdown.js v1.3.0
+ * http://twitter.github.com/bootstrap/javascript.html#dropdown
+ * ============================================================
+ * Copyright 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============================================================ */
+
+
 (function( $ ){
 
- /* DROPDOWN PLUGIN DEFINITION
-  * ========================== */
+  var d = 'a.menu, .dropdown-toggle'
 
   function clearMenus() {
-    $('a.menu').parent('li').removeClass('open')
+    $(d).parent('li').removeClass('open')
   }
 
-  $(window).bind("click", clearMenus)
+  $(function () {
+    $('html').bind("click", clearMenus)
+    $('body').dropdown( '[data-dropdown] a.menu, [data-dropdown] .dropdown-toggle' )
+  })
 
-  $.fn.dropdown = function ( options ) {
+  /* DROPDOWN PLUGIN DEFINITION
+   * ========================== */
+
+  $.fn.dropdown = function ( selector ) {
     return this.each(function () {
-      $(this).delegate('a.menu', 'click', function (e) {
+      $(this).delegate(selector || d, 'click', function (e) {
+        var li = $(this).parent('li')
+          , isActive = li.hasClass('open')
+
         clearMenus()
-        $(this).parent('li').toggleClass('open')
+        !isActive && li.toggleClass('open')
         return false
       })
     })
   }
 
-})( jQuery || ender )
+})( window.jQuery || window.ender )
