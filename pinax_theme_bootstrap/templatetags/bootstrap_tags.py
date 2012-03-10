45b8cac684295bf48f0331dd53832c7fec9fbae3
Luke Hatcher <lukeman@gmail.com>
2012-03-10 11:43:14 -0800

    render checkboxinputs properly

--- pinax_theme_bootstrap/templatetags/bootstrap_tags.py
+++ pinax_theme_bootstrap/templatetags/bootstrap_tags.py
@@ -1,3 +1,5 @@
+from django.forms.forms import BoundField
+from django.forms.widgets import CheckboxInput
 from django.template import Context
 from django.template.loader import get_template
 from django import template
@@ -11,3 +13,10 @@ def as_bootstrap(form):
     template = get_template("bootstrap/form.html")
     c = Context({"form": form})
     return template.render(c)
+
+
+@register.filter
+def is_checkbox(value):
+    if not isinstance(value, BoundField):
+        return False
+    return isinstance(value.field.widget, CheckboxInput)
