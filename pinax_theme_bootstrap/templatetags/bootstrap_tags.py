db60f6d693d301ccef5f098711b78eba90a2dcf0
Luke Hatcher <lukeman@gmail.com>
2012-03-13 18:14:30 -0700

    add rendering for radio and checkbox selects

--- pinax_theme_bootstrap/templatetags/bootstrap_tags.py
+++ pinax_theme_bootstrap/templatetags/bootstrap_tags.py
@@ -1,5 +1,5 @@
 from django.forms.forms import BoundField
-from django.forms.widgets import CheckboxInput
+from django.forms.widgets import CheckboxInput, RadioSelect
 from django.template import Context
 from django.template.loader import get_template
 from django import template
@@ -20,3 +20,9 @@ def is_checkbox(value):
     if not isinstance(value, BoundField):
         return False
     return isinstance(value.field.widget, CheckboxInput)
+
+@register.filter
+def is_radio(value):
+    if not isinstance(value, BoundField):
+        return False
+    return isinstance(value.field.widget, RadioSelect)
