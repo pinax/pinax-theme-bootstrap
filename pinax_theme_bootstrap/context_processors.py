c459dc273c729ca1a194b9f47b90ef0b4aa60687
Luke Hatcher <lukeman@gmail.com>
2013-06-04 10:12:26 -0400

    re-fix context processor

--- pinax_theme_bootstrap/context_processors.py
+++ pinax_theme_bootstrap/context_processors.py
@@ -5,8 +5,8 @@ from pinax_theme_bootstrap.conf import settings
 
 def theme(request):
     ctx = {
-        "THEME_ADMIN_URL": settings.ADMIN_URL,
-        "THEME_CONTACT_EMAIL": settings.CONTACT_EMAIL,
+        "THEME_ADMIN_URL": settings.THEME_ADMIN_URL,
+        "THEME_CONTACT_EMAIL": settings.THEME_CONTACT_EMAIL,
     }
     
     if Site._meta.installed:
