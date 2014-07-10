cf9ccdf5c6df8983f094be4ddbc3844e6c761185
James Tauber <jtauber@jtauber.com>
2014-07-10 00:27:04 -0400

    PEP8 nits

--- pinax_theme_bootstrap/conf.py
+++ pinax_theme_bootstrap/conf.py
@@ -1,12 +1,10 @@
-from django.conf import settings
-
 from appconf import AppConf
 
 
 class ThemeAppConf(AppConf):
-    
+
     ADMIN_URL = "admin:index"
     CONTACT_EMAIL = "support@example.com"
-    
+
     class Meta:
         prefix = "theme"
--- pinax_theme_bootstrap/context_processors.py
+++ pinax_theme_bootstrap/context_processors.py
@@ -8,12 +8,12 @@ def theme(request):
         "THEME_ADMIN_URL": settings.THEME_ADMIN_URL,
         "THEME_CONTACT_EMAIL": settings.THEME_CONTACT_EMAIL,
     }
-    
+
     if Site._meta.installed:
         site = Site.objects.get_current()
         ctx.update({
             "SITE_NAME": site.name,
             "SITE_DOMAIN": site.domain
         })
-    
+
     return ctx
