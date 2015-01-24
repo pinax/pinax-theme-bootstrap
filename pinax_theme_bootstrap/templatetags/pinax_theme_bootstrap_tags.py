bc7d6f7d46a5743b7d61e82e0e0962fdf5cc20f0
Jacob Wegner <jacobwegner@gmail.com>
2015-01-24 12:47:53 -0600

    Retrieve message tags via get_message_tags template tag

--- pinax_theme_bootstrap/templatetags/pinax_theme_bootstrap_tags.py
+++ pinax_theme_bootstrap/templatetags/pinax_theme_bootstrap_tags.py
@@ -1,3 +1,13 @@
 from django import template
 
 register = template.Library()
+
+
+@register.simple_tag()
+def get_message_tags(message):
+    """
+    Returns tags for a message
+    """
+    if message.tags:
+        return u"alert-{tags}".format(tags=message.tags)
+    return u""
