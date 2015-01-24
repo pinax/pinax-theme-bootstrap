2cb03ff8c3d21f36b95103eaf9ae0fb3e43077bd
Jacob Wegner <jacobwegner@gmail.com>
2015-01-24 13:54:32 -0600

    Allow for lazy translation of message tags

--- pinax_theme_bootstrap/templatetags/pinax_theme_bootstrap_tags.py
+++ pinax_theme_bootstrap/templatetags/pinax_theme_bootstrap_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.contrib.messages.utils import get_level_tags
+from django.utils.encoding import force_text
 
 
 LEVEL_TAGS = get_level_tags()
@@ -15,14 +16,15 @@ def get_message_tags(message):
 
     Messages in Django >= 1.7 have a message.level_tag attr
     """
-    level_tag = LEVEL_TAGS[message.level]
+    level_tag = force_text(LEVEL_TAGS.get(message.level, ''), strings_only=True)
     if level_tag == u"error":
         level_tag = u"danger"
 
     alert_level_tag = u"alert-{tag}".format(tag=level_tag)
 
     tags = [alert_level_tag]
-    if message.extra_tags:
-        tags.append(message.extra_tags)
+    extra_tags = force_text(message.extra_tags, strings_only=True)
+    if extra_tags:
+        tags.append(extra_tags)
 
     return u" ".join(tags)
