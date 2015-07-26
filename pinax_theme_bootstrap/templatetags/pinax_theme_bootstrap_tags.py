e1568a02379e818f267a79cabb7e43f91c7684cf
Hiroshi Miura <miurahr@linux.com>
2015-07-26 09:36:54 +0900

    unicode string in py2_3 compat way
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- pinax_theme_bootstrap/templatetags/pinax_theme_bootstrap_tags.py
+++ pinax_theme_bootstrap/templatetags/pinax_theme_bootstrap_tags.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django import template
 from django.contrib.messages.utils import get_level_tags
 from django.utils.encoding import force_text
@@ -17,22 +18,22 @@ def get_message_tags(message):
     Messages in Django >= 1.7 have a message.level_tag attr
     """
     level_tag = force_text(LEVEL_TAGS.get(message.level, ''), strings_only=True)
-    if level_tag == u"error":
+    if level_tag == "error":
         # Alias the error tag as danger, since .alert-error no longer exists
         # in Bootstrap 3
-        level_tag = u"danger"
+        level_tag = "danger"
 
     if level_tag:
-        alert_level_tag = u"alert-{tag}".format(tag=level_tag)
+        alert_level_tag = "alert-{tag}".format(tag=level_tag)
     else:
         alert_level_tag = None
 
     extra_tags = force_text(message.extra_tags, strings_only=True)
 
     if extra_tags and alert_level_tag:
-        return u' '.join([extra_tags, alert_level_tag])
+        return ' '.join([extra_tags, alert_level_tag])
     elif extra_tags:
         return extra_tags
     elif alert_level_tag:
         return alert_level_tag
-    return u''
+    return ''
